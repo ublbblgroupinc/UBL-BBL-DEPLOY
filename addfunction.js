80f1ccc60da2f1bb24a7924c7d8821cbedc2567c
Jesse Chan <z5477899@ad.unsw.edu.au>
2025-03-07 15:58:21 +1100

    npm jest test packeages installed and function and test file example

--- addfunction.js
+++ addfunction.js
@@ -1,7 +1,6 @@
-console.log('hello world!')
 
 function add (a, b) {
   return a + b
 }
 
-console.log('2 + 3 = %d', add(2, 3))
+module.exports = add
--- addfunction.test.js
+++ addfunction.test.js
@@ -1,9 +1,11 @@
-import { add } from './addfunction.js'
+const add = require('./addfunction')
 
 test('check add function works properly', () => {
-  expect(add(2, 3)).toEqual(5);
+  expect(add(2, 3)).toBe(5);
 });
 
 test('adds -1 + 1 to equal 0', () => {
   expect(add(-1, 1)).toBe(0);
 });
+
+// to run test use "npm test"
