66bf4f3d1bb6523288b14a6d24246bf3ac29b2fa
jesse640 <z5477899@ad.unsw.edu.au>
2025-03-07 17:36:07 +1100

    check test (#6)
    * check test
    
    * fix lint
    
    * fix lint

--- addfunction.test.js
+++ addfunction.test.js
@@ -1,15 +1,15 @@
 const add = require('./addfunction')
 
 test('check add function works properly', () => {
-  expect(add(2, 3)).toBe(5);
-});
+  expect(add(2, 3)).toBe(5)
+})
 
 test('adds -1 + 1 to equal 0', () => {
-  expect(add(-1, 1)).toBe(0);
-});
+  expect(add(-1, 1)).toBe(0)
+})
 
 test('add 5 and 6', () => {
-  expect(add(5, 6)).toBe(11);
+  expect(add(5, 6)).toBe(11)
 })
 
 // to run test use "npm test"
