e0638c134d76729427815fb64d5088c258ffa080
tkdaddy <167506267+tkdaddy@users.noreply.github.com>
2025-03-08 15:27:41 +1100

    Update server.js

--- server.js
+++ server.js
@@ -1,21 +1,21 @@
-const app = require('express')();
+const app = require('express')()
 const mongoose = require('mongoose')
-const PORT = 3000;
+const PORT = 3000
 
 app.get('/', (req, res) => {
-    res.send('Hello node api boss');
+  res.send('Hello node api boss')
 })
 
 app.get('/test', (req, res) => {
-    res.send(' my friend');
+  res.send(' my friend')
 })
 
 mongoose.connect('mongodb+srv://admin:12345abcde@invoicedatabase.owzuo.mongodb.net/?retryWrites=true&w=majority&appName=InvoiceDataBase')
 .then(() => {
-    console.log('Connected to DB')
-    app.listen(PORT, () => {
-        console.log(`Server is running on port ${PORT}`);
-    })
+  console.log('Connected to DB')
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+  })
 }).catch((error) => {
-    console.log(error)
+  console.log(error)
 })
