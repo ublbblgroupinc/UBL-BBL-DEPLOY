801beae77e4772df5b32ee51841e33d69964e88c
tkdaddy <167506267+tkdaddy@users.noreply.github.com>
2025-03-08 15:24:31 +1100

    Update InvoiceModel.js

--- models/InvoiceModel.js
+++ models/InvoiceModel.js
@@ -1,10 +1,1 @@
-// const mongoose = require('mongoose')
 
-// const mongooseSchema = mongoose.Schema(
-//     {
-//         id: {
-//             type: String,
-//             required: [true, "Enter invoice id"]
-//         }
-//     }
-// )
