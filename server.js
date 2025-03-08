3f92bbad9a1d5f8eeee51979eaf7e759bc4ca947
tkdaddy <167506267+tkdaddy@users.noreply.github.com>
2025-03-08 15:33:23 +1100

    Update server.js
    Fixed linting again

--- server.js
+++ server.js
@@ -11,11 +11,11 @@ app.get('/test', (req, res) => {
 })
 
 mongoose.connect('mongodb+srv://admin:12345abcde@invoicedatabase.owzuo.mongodb.net/?retryWrites=true&w=majority&appName=InvoiceDataBase')
-.then(() => {
-  console.log('Connected to DB')
-  app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
+  .then(() => {
+    console.log('Connected to DB')
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`)
+    })
+  }).catch((error) => {
+    console.log(error)
   })
-}).catch((error) => {
-  console.log(error)
-})
