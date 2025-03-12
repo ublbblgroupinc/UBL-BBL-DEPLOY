23fb23f3c07b656f176b51cb46ca465b16801c0a
Jesse Chan <z5477899@ad.unsw.edu.au>
2025-03-12 22:15:09 +1100

    restructured code and wrote tests for signup route

---
+++ __tests__/user-tests/signup.test.js
@@ -0,0 +1,101 @@
+const request = require('supertest')
+const { app, server } = require('../../app/server')
+const User = require('../../app/models/UsersModel')
+const mongoose = require('mongoose') // Import mongoose for DB teardown
+
+describe('POST /user/signup', () => {
+  afterAll(async () => {
+    await mongoose.connection.close() // Ensure DB connection is closed
+    server.close()
+  })
+
+  it('should create a new user and return a success response', async () => {
+    const newUser = {
+      username: 'testuser',
+      email: 'testuser@example.com',
+      password: 'Password123*'
+    }
+
+    const response = await request(app)
+      .post('/user/signup')
+      .send(newUser)
+      .expect(201)
+
+    expect(response.body.message).toBe('Signup successful')
+
+    await User.findByIdAndDelete(response.body.user._id) // Remove test user
+  })
+
+  it('should return an error when not all fields are filled out i.e. no username provided', async () => {
+    const newUser = {
+      email: 'testuser@example.com',
+      password: 'Password123*'
+    }
+
+    const response = await request(app)
+      .post('/user/signup')
+      .send(newUser)
+      .expect(400)
+
+    expect(response.body.error).toBe('Missing fields')
+  })
+
+  it('should return error when email is in wrong format', async () => {
+    const newUser = {
+      username: 'testuser',
+      email: 'thisIsNotAnEmail',
+      password: 'Password123*'
+    }
+
+    const response = await request(app)
+      .post('/user/signup')
+      .send(newUser)
+      .expect(400)
+
+    expect(response.body.error).toBe('Invalid email')
+  })
+
+  it('should return error when password is invalid i.e. min 8 characters, 1 capital, number and special character', async () => {
+    const newUser = {
+      username: 'testuser',
+      email: 'testuser@example.com',
+      password: 'badpassword'
+    }
+
+    const response = await request(app)
+      .post('/user/signup')
+      .send(newUser)
+      .expect(400)
+
+    expect(response.body.error).toBe(
+      'Password must be at least 8 characters long, include 1 uppercase,' +
+      ' 1 number, and 1 special character.'
+    )
+  })
+
+  it('should return error when user already exists', async () => {
+    const newUser = {
+      username: 'testuser',
+      email: 'testuser@example.com',
+      password: 'Password123*'
+    }
+
+    // signup new user
+    const response = await request(app)
+      .post('/user/signup')
+      .send(newUser)
+      .expect(201)
+
+    expect(response.body.message).toBe('Signup successful')
+
+    // signup user again with same email and username
+    const response2 = await request(app)
+      .post('/user/signup')
+      .send(newUser)
+      .expect(400)
+
+    expect(response2.body.error).toBe('User already exists')
+
+    await User.findByIdAndDelete(response.body.user._id) // Remove test user
+  })
+})
