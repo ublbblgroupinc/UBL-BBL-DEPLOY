ce64a35b75c4dfb3f3d94809be181d682b27ebee
Jake Takada Wong <z5479348@ad.unsw.edu.au>
2025-03-15 23:10:26 +1100

    more linting fixes

--- app/main/services/userService.js
+++ app/main/services/userService.js
@@ -47,8 +47,8 @@ exports.login = async (userData) => {
   if (!isMatch) throw new Error('Incorrect password')
 
   // Generate token
-  const token = jwt.sign({user}, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '2h' })
-  return token;
+  const token = jwt.sign({ user }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '2h' })
+  return token
 }
 
 exports.getUserInfoVerify = async (email) => {
@@ -61,23 +61,23 @@ exports.getUserInfoVerify = async (email) => {
 exports.putInfo = async (userData) => {
   let { email, newusername, newemail, newpassword } = userData
 
-  //checks if user exists
+  // checks if user exists
   let user = await User.findOne({ email })
   if (!user) throw new Error('User doesn\'t exist')
 
   if (newusername != null) {
-    //error checking
-    if ( user.username == newusername ) throw new Error('New username should not be current username')
+    // error checking
+    if (user.username == newusername) throw new Error('New username should not be current username')
 
     const username = newusername
     const existingUsername = await User.findOne({ username })
     if (existingUsername) throw new Error('Username already exists')
-    //updating username
-    await User.updateOne({email: user.email}, {$set:{username: newusername}})
+    // updating username
+    await User.updateOne({ email: user.email }, { $set: { username: newusername } })
   }
 
   if (newpassword != null) {
-    //error checking
+    // error checking
     const isMatch = await bcrypt.compare(newpassword, user.password)
     if (isMatch) throw new Error('New password should not be current password')
 
@@ -87,27 +87,27 @@ exports.putInfo = async (userData) => {
             '1 number, and 1 special character.'
       )
     }
-    //updating password
+    // updating password
     const hashedPassword = await bcrypt.hash(newpassword, 10)
-    await User.updateOne({email: user.email}, {$set:{password: hashedPassword}})
+    await User.updateOne({ email: user.email }, { $set: { password: hashedPassword } })
   }
 
   if (newemail != null) {
-    //error checking
-    if ( user.email == newemail ) throw new Error('New email should not be current email')
+    // error checking
+    if (user.email == newemail) throw new Error('New email should not be current email')
 
     email = newemail
     const existingEmail = await User.findOne({ email })
     if (existingEmail) throw new Error('Email is already being used')
 
     if (!isValidEmail(newemail)) throw new Error('Invalid email')
-    //updating uer
-    await User.updateOne({email: user.email}, {$set:{email: newemail}})
-    user = await User.findOne({ email }) //changes user so token can update
+    // updating uer
+    await User.updateOne({ email: user.email }, { $set: { email: newemail } })
+    user = await User.findOne({ email }) // changes user so token can update
   }
 
-  const token = jwt.sign({user}, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '2h' })
-  return token;
+  const token = jwt.sign({ user }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '2h' })
+  return token
 }
 
 /// /////////////// HELPER FUNCTIONS //////////////////////////////////////
